51890c1d6649568abe034e8eda658d50220049de
David Rivera Morales <driveramorales46@gmail.com>
2022-10-24 00:22:46 -0500

    File structure for testing with python

---
+++ python/valid-anagram/exercise.py
@@ -0,0 +1,18 @@
+import unittest
+from typing import Counter
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return Counter(s) == Counter(t)
+
+
+class TestAnagram(unittest.TestCase):
+    def test_anagram(self):
+        self.assertTrue(Solution().isAnagram("anagram", "nagaram"))
+        self.assertFalse(Solution().isAnagram("rat", "car"))
