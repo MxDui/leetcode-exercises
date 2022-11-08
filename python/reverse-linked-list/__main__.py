8e02a18582d75ef464c255a2573e866b3e2558e3
David Rivera Morales <driveramorales46@gmail.com>
2022-11-08 12:59:58 -0600

    finished reverse linked list

---
+++ python/reverse-linked-list/__main__.py
@@ -0,0 +1,34 @@
+import unittest
+from typing import List
+from typing import Optional
+
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        prev = None
+
+        curr = head 
+
+        while curr != None:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
+
+class TestReverseList(unittest.TestCase):
+    def test_reverse_list(self):
+        
+        self.assertEqual(Solution().reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))).next.next.next.next.val, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+    
