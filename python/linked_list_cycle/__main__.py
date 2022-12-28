9bee82e4a7fb7c9edabcef1b480c9756d7a3ed94
David Rivera Morales <driveramorales46@gmail.com>
2022-12-27 21:13:00 -0600

    linked list cycle finished

---
+++ python/linked_list_cycle/__main__.py
@@ -0,0 +1,35 @@
+import unittest
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if (slow == fast):
+                return True
+
+        return False
+
+
+class TestSolution(unittest.TestCase):
+    def test_hasCycle(self):
+        head = ListNode(3)
+        head.next = ListNode(2)
+        head.next.next = ListNode(0)
+        head.next.next.next = ListNode(-4)
+        head.next.next.next.next = head.next
+
+        self.assertTrue(Solution().hasCycle(head))
+
+if __name__ == '__main__':
+    unittest.main()
