fc1b8d069d4956b57ef1186fb7ecf435d4eec1a0
David Rivera Morales <driveramorales46@gmail.com>
2023-01-01 19:18:16 -0600

    finished diameter of a tree

---
+++ python/diameter_binary_tree/__main__.py
@@ -0,0 +1,37 @@
+import unittest
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            res[0] = max(res[0],2 + left + right)
+
+            return 1+max(left,right)
+
+        dfs(root)
+        return res[0]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        self.assertEqual(Solution().diameterOfBinaryTree(root), 3)
+
+if __name__ == '__main__':
+    unittest.main()
