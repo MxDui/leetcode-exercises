428e461dfe8575639cd213b5ab9eb3f65da63ad1
David Rivera Morales <driveramorales46@gmail.com>
2023-05-27 23:13:27 -0600

    finished multiply string

---
+++ python/multiply_strings/__main__.py
@@ -0,0 +1,35 @@
+import unittest
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += res[i1 + i2] // 10
+                res[i1 + i2] = res[i1 + i2] % 10
+
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
+
+
+class Test(unittest.TestCase):
+    
+        def test_multiply(self):
+    
+            s = Solution()
+            self.assertEqual(s.multiply("2", "3"), "6")
+            self.assertEqual(s.multiply("123", "456"), "56088")
+            self.assertEqual(s.multiply("123456789", "987654321"), "121932631112635269")
+            self.assertEqual(s.multiply("123456789", "0"), "0")
+
+
+if __name__ == '__main__':
+    unittest.main()
